fa2329d0b10a0fd10efc5606472fd036f0e23ca8
Mircea Alexandru <malex@malex-srv.(none)>
2012-12-09 00:23:19 +0200

    Added unit test for cron plugin. Fixes #2

--- test/cron.test.js
+++ test/cron.test.js
@@ -2,9 +2,12 @@
 
 var seneca   = require('seneca')
 var cron     = require('../lib/cron')
-
+var assert   = require('assert')
 var si
 var jobid
+
+var counter = 0
+var counter2 = 0
 
 function initSeneca(){
   si = seneca({"log":"print"})
@@ -14,7 +17,7 @@ function initSeneca(){
 
 function doConfig(){
   si.act({role:'cron',cmd:'addjob', time:'* * * * * *', act: function(){
-    si.log('tick-tick');
+    si.log('tick-tick', counter++);
   }, after: function(){
     si.log('Now I will end');
   }, timezone: null}, function(error, id){
@@ -24,23 +27,41 @@ function doConfig(){
   })
 
   function setTimeoutTests(){
+    // stop after 10s
     setTimeout(function(){
       si.log('exit');
       si.act({role:'cron',cmd:'stopjob', id: jobid}, function(){
         si.log('stop cron job', jobid)
         si.close();
       })
-
     },(10*1000));
 
+    // test counter value
     setTimeout(function(){
-      si.log('exit');
+      si.log('test counter value. Cron job was stopped please wait...');
+      assert(counter > 7)
+      counter2 = counter
+    },(11*1000));
+
+    // test counter stops from sec 10
+    setTimeout(function(){
+      si.log('test counter was stopped');
+      assert.equal(counter2, counter)
+    },(19*1000));
+
+    setTimeout(function(){
+      si.log('start again the job', jobid);
       si.act({role:'cron',cmd:'startjob', id: jobid}, function(){
         si.log('start cron job', jobid)
         si.close();
       })
-
     },(20*1000));
+
+    // test counter increase value
+    setTimeout(function(){
+      si.log('test counter was restarted');
+      assert(counter2 < counter)
+    },(25*1000));
 
     setTimeout(function(){
       si.log('exit');
@@ -48,7 +69,6 @@ function doConfig(){
         si.log('cron plugin closed')
         si.close();
       })
-
     },(30*1000));
   }
 }
